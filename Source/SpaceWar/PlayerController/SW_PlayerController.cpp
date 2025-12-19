7d4ee89b0c8f19be0025fff65864f39b68c771e6
Z-HongYuan <1939218189@qq.com>
2025-12-19 14:54:56 +0800

    feat(input): 实现玩家控制器输入系统
    - 添加 Enhanced Input 子系统支持
    - 实现移动输入动作绑定
    - 创建输入映射上下文管理方法
    - 支持动态添加和移除输入上下文
    - 集成 pawn 移动控制逻辑
    - 提供蓝图可调用的输入切换接口

---
+++ Source/SpaceWar/PlayerController/SW_PlayerController.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SW_PlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+ASW_PlayerController::ASW_PlayerController()
+{
+}
+
+void ASW_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASW_PlayerController::OnPossess(APawn* APawn)
+{
+	Super::OnPossess(APawn);
+
+	ChangeInputContext(DefaultInputContext, 0, true);
+}
+
+void ASW_PlayerController::OnUnPossess()
+{
+	Super::OnUnPossess();
+
+	ChangeInputContext(DefaultInputContext, 0, false);
+}
+
+void ASW_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		Input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::PawnMove);
+	}
+}
+
+void ASW_PlayerController::PawnMove(const FInputActionValue& Value)
+{
+	if (!GetPawn()) return;
+	GetPawn()->AddMovementInput(Value.Get<FVector>());
+}
+
+void ASW_PlayerController::ChangeInputContext(UInputMappingContext* Context, int32 Priority, bool bIsAdd)
+{
+	if (!IsLocalController()) return;
+
+	if (const ULocalPlayer* LP = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			bIsAdd ? Subsystem->AddMappingContext(Context, Priority) : Subsystem->RemoveMappingContext(Context);
+		}
+	}
+}
---
+++ Source/SpaceWar/PlayerController/SW_PlayerController.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SW_PlayerController.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEWAR_API ASW_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ASW_PlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void OnPossess(APawn* APawn) override;
+
+	virtual void OnUnPossess() override;
+
+	virtual void SetupInputComponent() override;
+
+	void PawnMove(const FInputActionValue& Value);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input|Default", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputMappingContext> DefaultInputContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input|Default", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> MoveAction;
+
+public:
+	/*
+	 * 添加输入上下文(已经默认调用
+	 */
+	UFUNCTION(blueprintCallable)
+	void ChangeInputContext(UInputMappingContext* Context, int32 Priority, bool bIsAdd = true);
+};
