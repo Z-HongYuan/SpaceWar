f44a5bdcf282bbaaf337dd9a1e644b7d35f032dd
Z-HongYuan <1939218189@qq.com>
2025-12-19 11:24:12 +0800

    feat(spacewar): 添加自定义漂浮pawn移动组件

---
+++ Source/SpaceWar/Components/SW_FloatingPawnMovement.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SW_FloatingPawnMovement.h"
+
+
+USW_FloatingPawnMovement::USW_FloatingPawnMovement()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void USW_FloatingPawnMovement::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void USW_FloatingPawnMovement::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/SpaceWar/Components/SW_FloatingPawnMovement.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "SW_FloatingPawnMovement.generated.h"
+
+/*
+ * 自定义的漂浮组件
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SPACEWAR_API USW_FloatingPawnMovement : public UFloatingPawnMovement
+{
+	GENERATED_BODY()
+
+public:
+	USW_FloatingPawnMovement();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+public:
+};
