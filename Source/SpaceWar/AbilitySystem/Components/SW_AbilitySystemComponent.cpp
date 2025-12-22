7cfe1657267e25040812fa605a309afc88cf1a73
Z-HongYuan <1939218189@qq.com>
2025-12-22 11:07:21 +0800

    feat(spacewar): 创建基础能力系统组件,添加模块依赖

---
+++ Source/SpaceWar/AbilitySystem/Components/SW_AbilitySystemComponent.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SW_AbilitySystemComponent.h"
+
+
+USW_AbilitySystemComponent::USW_AbilitySystemComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void USW_AbilitySystemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void USW_AbilitySystemComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
---
+++ Source/SpaceWar/AbilitySystem/Components/SW_AbilitySystemComponent.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "SW_AbilitySystemComponent.generated.h"
+
+/*
+ * 
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SPACEWAR_API USW_AbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+
+public:
+	USW_AbilitySystemComponent();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+public:
+};
