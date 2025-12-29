afdca0591c3f1460a8642ec8a5a09d5db8bb6833
Z-HongYuan <1939218189@qq.com>
2025-12-29 17:10:45 +0800

    feat(building): 添加战斗Actor组件
    - 创建了SW_CombatActor类继承自SW_BuildingActor
    - 集成AbilitySystemComponent和StateTreeComponent
    - 实现基于GameplayTag的战斗状态判断逻辑
    - 添加BeginPlay中自动启动状态树功能
    - 支持通过TAG_LevelState_Combat标签控制行为树执行

---
+++ Source/SpaceWar/Building/SW_CombatActor.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SW_CombatActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "SpaceWar/Data/SW_GameplayTags.h"
+
+
+ASW_CombatActor::ASW_CombatActor() :
+	StateTreeComponent(CreateDefaultSubobject<UStateTreeComponent>(TEXT("StateTreeComponent")))
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	StateTreeComponent->SetStartLogicAutomatically(false);
+}
+
+void ASW_CombatActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APawn* Pawn = GetOwner<APawn>();
+
+	if (Pawn && Pawn->Implements<UAbilitySystemInterface>())
+	{
+		UAbilitySystemComponent* ASC = Cast<IAbilitySystemInterface>(Pawn)->GetAbilitySystemComponent();
+
+		if (ASC && ASC->HasMatchingGameplayTag(TAG_LevelState_Combat))
+		{
+			// Pawn 拥有该 GameplayTag,执行行为树
+			StateTreeComponent->StartLogic();
+		}
+	}
+}
+
+void ASW_CombatActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/SpaceWar/Building/SW_CombatActor.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SW_BuildingActor.h"
+#include "Components/StateTreeComponent.h"
+#include "SW_CombatActor.generated.h"
+
+/*
+ * 战斗Actor
+ * 通过GameplayTags判断是否激活状态树
+ * 
+ */
+UCLASS()
+class SPACEWAR_API ASW_CombatActor : public ASW_BuildingActor
+{
+	GENERATED_BODY()
+
+public:
+	ASW_CombatActor();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(Category=Pawn, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UStateTreeComponent> StateTreeComponent;
+
+public:
+};
