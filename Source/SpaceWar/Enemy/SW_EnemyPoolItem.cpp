89be2bbc253c43f8520e2aeed0de4e8ed1b71b6c
Z-HongYuan <1939218189@qq.com>
2026-01-15 10:21:05 +0800

    新添加一个专门用于对象池使用的EnemyPoolItemActor

---
+++ Source/SpaceWar/Enemy/SW_EnemyPoolItem.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SW_EnemyPoolItem.h"
+
+#include "NiagaraFunctionLibrary.h"
+#include "SpaceWar/Data/SW_PoolType.h"
+#include "SpaceWar/Subsystem/SW_EnemyManager.h"
+
+
+class UNiagaraSystem;
+
+ASW_EnemyPoolItem::ASW_EnemyPoolItem() :
+	StateTreeComponent(CreateDefaultSubobject<UStateTreeComponent>(TEXT("StateTreeComponent"))),
+	BoxComponent(CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"))),
+	StaticMeshComponent(CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent")))
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	StateTreeComponent->SetStartLogicAutomatically(false);
+
+	SetRootComponent(BoxComponent);
+	BoxComponent->SetCollisionProfileName(TEXT("Enemy"));
+	BoxComponent->SetGenerateOverlapEvents(false);
+
+	StaticMeshComponent->SetupAttachment(BoxComponent);
+	StaticMeshComponent->SetCollisionProfileName(TEXT("NoCollision"));
+}
+
+void ASW_EnemyPoolItem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASW_EnemyPoolItem::ActiveItem(const FEnemyItemParam& InEnemyItemParam)
+{
+	Health = InEnemyItemParam.InHealth;
+	SetActorTransform(InEnemyItemParam.InSpawnTransform);
+	SetActorHiddenInGame(false);
+
+	SetActorEnableCollision(true);
+	BoxComponent->SetCollisionProfileName(TEXT("Enemy"));
+
+	StateTreeComponent->RestartLogic();
+}
+
+void ASW_EnemyPoolItem::DeactiveItem()
+{
+	//播放死亡特效||优先于设置位置
+	if (Health <= 0.0f)
+		if (UNiagaraSystem* FX = DieFX.LoadSynchronous())
+			UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, FX, GetActorLocation(), GetActorRotation());
+
+	SetActorTransform(FTransform());
+	SetActorHiddenInGame(true);
+
+	SetActorEnableCollision(false);
+	BoxComponent->SetCollisionProfileName(TEXT("NoCollision"));
+
+	StateTreeComponent->StopLogic(TEXT("Deactive"));
+}
+
+float ASW_EnemyPoolItem::TakeDamage(float DamageAmount, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	const float DamageTaken = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	if (DamageTaken > 0.0f)
+	{
+		Health -= DamageTaken;
+		if (Health <= 0.0f)
+		{
+			EnemyManager->ReturnEnemy(this);
+		}
+	}
+
+	//生成受击粒子特效
+	if (UNiagaraSystem* FX = HitFX.LoadSynchronous())
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, FX, GetActorLocation(), GetActorRotation());
+
+
+	return DamageTaken;
+}
---
+++ Source/SpaceWar/Enemy/SW_EnemyPoolItem.h
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "Components/StateTreeComponent.h"
+#include "GameFramework/Actor.h"
+#include "SW_EnemyPoolItem.generated.h"
+
+struct FEnemyItemParam;
+class UNiagaraSystem;
+class USW_EnemyManager;
+
+UCLASS()
+class SPACEWAR_API ASW_EnemyPoolItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASW_EnemyPoolItem();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(Category=SpaceWar, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UStateTreeComponent> StateTreeComponent;
+
+	UPROPERTY(Category=SpaceWar, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent> BoxComponent;
+
+	UPROPERTY(Category=SpaceWar, VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+
+public:
+	UPROPERTY()
+	TWeakObjectPtr<USW_EnemyManager> EnemyManager;
+
+	/*
+	 * 变换
+	 * 显示隐藏
+	 * 碰撞
+	 * 状态树
+	 * 标签配置(是否需要设置标志位,用GameplayTag)
+	 * 如何过滤场景查询,避免未启用的时候都会锁定
+	 */
+	void ActiveItem(const FEnemyItemParam& InEnemyItemParam);
+	void DeactiveItem();
+
+	/*
+	 * 处理伤害
+	 */
+	virtual float TakeDamage(float DamageAmount, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+	UPROPERTY(BlueprintReadWrite, meta=(ExposeOnSpawn), Category = "Enemy")
+	float Health = 500;
+
+	// 被攻击时的特效
+	UPROPERTY(Category=SpaceWar, EditDefaultsOnly)
+	TSoftObjectPtr<UNiagaraSystem> HitFX;
+
+	// 死亡时的特效
+	UPROPERTY(Category=SpaceWar, EditDefaultsOnly)
+	TSoftObjectPtr<UNiagaraSystem> DieFX;
+};
