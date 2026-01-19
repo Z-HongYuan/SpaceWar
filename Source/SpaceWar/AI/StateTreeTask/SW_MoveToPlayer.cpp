153c6b38c85306fb5f5bff0b076c0c51585c1871
Z-HongYuan <1939218189@qq.com>
2026-01-19 16:34:17 +0800

    feat(AI): 添加向玩家移动的状态树任务

---
+++ Source/SpaceWar/AI/StateTreeTask/SW_MoveToPlayer.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SW_MoveToPlayer.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+EStateTreeRunStatus USW_MoveToPlayer::EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition)
+{
+	return Super::EnterState(Context, Transition);
+}
+
+EStateTreeRunStatus USW_MoveToPlayer::Tick(FStateTreeExecutionContext& Context, const float DeltaTime)
+{
+	RunStatus = Super::Tick(Context, DeltaTime);
+
+	const FVector ContextLocation = ContextObject->GetActorLocation();
+	const FVector TargetLocation = TargetObject->GetActorLocation();
+
+	ContextObject->SetActorLocationAndRotation(
+		ContextLocation + MoveSpeed * DeltaTime * (TargetLocation - ContextLocation).GetSafeNormal(),
+		UKismetMathLibrary::FindLookAtRotation(ContextLocation, TargetLocation),
+		true);
+
+	if ((TargetLocation - ContextLocation).SizeSquared2D() <= DistanceRange)
+	{
+		RunStatus = EStateTreeRunStatus::Succeeded;
+	}
+	else
+	{
+		RunStatus = EStateTreeRunStatus::Running;
+	}
+
+	return RunStatus;
+}
---
+++ Source/SpaceWar/AI/StateTreeTask/SW_MoveToPlayer.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/StateTreeTaskBlueprintBase.h"
+#include "SW_MoveToPlayer.generated.h"
+
+/**
+ * 朝向Player进行移动
+ */
+UCLASS(meta = (DisplayName = "SW 向Player移动"))
+class SPACEWAR_API USW_MoveToPlayer : public UStateTreeTaskBlueprintBase
+{
+	GENERATED_BODY()
+
+public:
+	USW_MoveToPlayer(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+	{
+		bShouldCallTick = true; //启用Tick
+	}
+
+protected:
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) override;
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) override;
+
+private:
+public:
+	/*
+	 * 一般调用者为Enemy
+	 */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Context")
+	TObjectPtr<AActor> ContextObject;
+
+	/*
+	 * 一般是玩家
+	 */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Input")
+	TObjectPtr<AActor> TargetObject;
+
+	/*
+	 * 参数
+	 */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Parameter")
+	float MoveSpeed = 300.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Parameter")
+	float DistanceRange = 200.0f;
+};
