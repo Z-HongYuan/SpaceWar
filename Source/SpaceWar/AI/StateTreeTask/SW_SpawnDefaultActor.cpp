c9809ec980e48da7aedfdb811b3ab8c819235a14
Z-HongYuan <1939218189@qq.com>
2026-01-20 13:50:58 +0800

    feat(ai): 添加默认Actor生成状态树任务
    - 实现了SW_SpawnDefaultActor类用于在状态树中生成Actor
    - 添加了ContextObject、SpawnedClass和SpawnTransform属性
    - 设置了Owner和Instigator为Context的拥有者
    - 使用AdjustIfPossibleButAlwaysSpawn碰撞处理方法
    - 继承自UStateTreeTaskBlueprintBase基类
    - 提供了蓝图可读写的编辑接口

---
+++ Source/SpaceWar/AI/StateTreeTask/SW_SpawnDefaultActor.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SW_SpawnDefaultActor.h"
+
+EStateTreeRunStatus USW_SpawnDefaultActor::EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition)
+{
+	RunStatus = Super::EnterState(Context, Transition);
+	if (RunStatus == EStateTreeRunStatus::Failed) return RunStatus;
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = ContextObject->GetOwner();
+	SpawnParams.Instigator = Cast<APawn>(ContextObject->GetOwner());
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	ContextObject->GetWorld()->SpawnActor<AActor>(
+		SpawnedClass,
+		SpawnTransform,
+		SpawnParams
+	);
+
+	return EStateTreeRunStatus::Succeeded;
+}
---
+++ Source/SpaceWar/AI/StateTreeTask/SW_SpawnDefaultActor.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/StateTreeTaskBlueprintBase.h"
+#include "SW_SpawnDefaultActor.generated.h"
+
+/**
+ * 必定生成一个Actor
+ * Owner和Instigator都是Context的拥有者
+ */
+UCLASS()
+class SPACEWAR_API USW_SpawnDefaultActor : public UStateTreeTaskBlueprintBase
+{
+	GENERATED_BODY()
+
+public:
+	USW_SpawnDefaultActor(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+	{
+	}
+
+protected:
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) override;
+
+private:
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Context")
+	TObjectPtr<AActor> ContextObject;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Input")
+	TSubclassOf<AActor> SpawnedClass;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Parameter")
+	FTransform SpawnTransform;
+};
